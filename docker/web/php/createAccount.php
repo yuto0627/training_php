9c8abc66c2873cedd71e9b5911ad967055e1cbe1
山田史 <yamadachika@yamadachikanoMacBook-Air.local>
2022-05-13 11:03:52 +0900

    タスク2修正

--- docker/web/php/createAccount.php
+++ docker/web/php/createAccount.php
@@ -5,41 +5,42 @@
 </head>
 
 <body>
-    <div class="header_left">
-        <header class="header_letter">
+    <div class="header-left">
+        <header class="header-letter">
             Bulltin Board
         </header>
     </div>
 
-    <div class="return_button">
+    <div class="return-button">
         <input onclick="location.href='../../../index.php'" value="戻る">
     </div>
 
-    <div class="login_screen upper">
+    <div class="login-screen upper">
         <h1>Bulltin Board</h1>
         <p>新規追加画面</p>
     </div>
 
     <div class="back-square">
-        <div class="login_screen">
+        <div class="login-screen">
             <h2>新規追加</h2>
             <p>ユーザーIDとパスワードを登録してください。</p>
         </div>
 
-        <div class="createaccount_screen">
+        <div class="createaccount-screen">
             <form method="post" action="login">
                 <input type="text" placeholder="ユーザーID">
             </form>
         </div>
 
-        <div class="login_screen">
-                <input type="text" placeholder="パスワード">
-                <input type="text" placeholder="パスワード確認">
+        <div class="login-screen">
+            <input type="text" placeholder="パスワード">
+            <input type="text" placeholder="パスワード確認">
         </div>
 
-        <div class="login_button">
+        <div class="login-button">
             <button onclick="location.href='login'">登録する</button>
         </div>
     </div>
 </body>
+
 </html>
